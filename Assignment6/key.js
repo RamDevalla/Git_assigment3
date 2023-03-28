23af93b3feee0bbadde419a623d2673038f8569c
RamDevalla <ramdevalla777@gmail.com>
2023-03-28 09:22:36 +0530

    Assignment6 added

---
+++ Assignment6/key.js
@@ -0,0 +1,4 @@
+module.exports = {
+    // MONGOURI : "mongodb+srv://ramdevalla777:Mongo%4012345@cluster0.kxoohjc.mongodb.net/Blog", // For Cloud
+    MONGOURI : "mongodb://localhost:27017"          //Local MOngoDatabase
+}
